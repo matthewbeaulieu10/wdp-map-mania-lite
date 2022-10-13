18e6c97c05581abc188458e8515feb8b82244fbd
matthew <matthewbeaulieu10@gmail.com>
2022-10-12 23:45:53 -0500

    finished map mania lite

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -8,11 +8,34 @@ function changeColor() {
     }
 }
 
-var gMAP;
+var gMap;
 
 function initMap() {
     gMap = new google.maps.Map(document.getElementById("myMapID"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+      center: {lat:0,lng:0},
+      zoom: 3,
     });
-  }
+
+    var marker = new google.maps.Marker({position:{lat:41.970760,lng:-88.351590},map:gMap});
+    marker.setIcon('https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png')
+  
+    var infoWindow = new google.maps.InfoWindow({content:'my home'})
+    marker.addListener('click', function() {
+        infoWindow.open(gMap, marker);
+    })
+
+    google.maps.event.addListener(gMap, 'idle', function() {
+        updateGame()
+    })
+}
+
+function updateGame() {
+    var zoomLevel = gMap.getZoom()
+    var inBounds = false;
+
+    if (gMap.getBounds().contains({lat:41.970760,lng:-88.351590})) {
+        inBounds = true;
+    }
+
+    console.log("inBounds:"+inBounds+" zoomLevel:"+zoomLevel)
+}
